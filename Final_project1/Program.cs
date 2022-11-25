a9f7d6b62bfcc22abbd243bcd8f3a6ab839d116c
Stas <smaster950@gmail.com>
2022-11-25 11:10:16 +0300

    Add FillArray and PrintArray methods

--- Final_project1/Program.cs
+++ Final_project1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter size of your Array.");
+int size = int.Parse(Console.ReadLine() ?? "");
+
+string[] inputArray = new string[size];
+FillArray(inputArray);
+PrintArray(inputArray);
+
+void FillArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Enter element #{i + 1}: ");
+        array[i] = Console.ReadLine();
+        Console.WriteLine();
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
