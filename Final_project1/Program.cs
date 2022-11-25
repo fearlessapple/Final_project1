2d07af3dab9c880aabf1afe6cb6a70ecb2e90c4a
Stas <smaster950@gmail.com>
2022-11-25 11:20:49 +0300

    Add ChangeArray method

--- Final_project1/Program.cs
+++ Final_project1/Program.cs
@@ -1,17 +1,34 @@
-﻿Console.WriteLine("Enter size of your Array.");
+﻿Console.Write("Enter size of your Array: ");
 int size = int.Parse(Console.ReadLine() ?? "");
 
 string[] inputArray = new string[size];
+string[] newArray = new string[inputArray.Length];
+
 FillArray(inputArray);
 PrintArray(inputArray);
+ChangeArray(inputArray, newArray);
+PrintArray(newArray);
 
 void FillArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
         Console.Write($"Enter element #{i + 1}: ");
-        array[i] = Console.ReadLine();
+        array[i] = Console.ReadLine() ?? "";
         Console.WriteLine();
+    }
+}
+
+void ChangeArray(string[] firstArray, string[] secondArray)
+{
+    int count = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+        if (firstArray[i].Length <= 3)
+        {
+            secondArray[count] = firstArray[i];
+            count++;
+        }
     }
 }
 
